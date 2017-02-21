e7f7883bdd301ea5bf6cb21eda8cd16913886bc5
MLAgens <self@meredithagens.com>
2017-02-21 15:32:25 -0500

    mongo

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 'use strict';
-process.env.MONGOHQ_UR = 'mongodb://test1:Password@ds151279.mlab.com:51279/property';
+process.env.MONGOHQ_URL = 'mongodb://test1:Password@ds151279.mlab.com:51279/property';
 
 module.exports = {
   secure: {
@@ -10,7 +10,7 @@ module.exports = {
 
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || 'mongodb://test1:Password@ds151279.mlab.com:51279/property' || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
       user: '',
       pass: ''
