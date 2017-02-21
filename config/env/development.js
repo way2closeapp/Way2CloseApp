102641bd172f22ab271987ee0ca291f80690af03
MLAgens <self@meredithagens.com>
2017-02-21 12:47:44 -0500

    fixed

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 'use strict';
-process.env.MONGOHQ_UR ='mongodb://test1:Password@ds151279.mlab.com:51279/property';
+process.env.MONGOHQ_URL ='mongodb://test1:Password@ds151279.mlab.com:51279/property';
 
 //|| process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev'
 var defaultEnvConfig = require('./default');
