aa8965dc6a58a06a1fb012dc2b14e3da7c2096db
MLAgens <self@meredithagens.com>
2017-03-15 13:02:25 -0400

    articles

---
+++ modules/articles/client/config/articles.client.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('articles').run(['Menus',
+  function (Menus) {
+    // Add the articles dropdown item
+    Menus.addMenuItem('topbar', {
+      title: 'Create Property',
+      state: 'articles',
+      type: 'dropdown',
+      roles: ['admin']
+    });
+
+    // Add the dropdown list item
+    Menus.addSubMenuItem('topbar', 'articles', {
+      title: 'List Properties',
+      state: 'articles.list'
+    });
+
+    // Add the dropdown create item
+    Menus.addSubMenuItem('topbar', 'articles', {
+      title: 'Create Properties',
+      state: 'articles.create',
+      roles: ['admin']
+    });
+  }
+]);
