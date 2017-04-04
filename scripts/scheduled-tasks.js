93a44317c637a609e6c098e4706bc89680d1bc69
gcaudill <gcaudill>
2017-04-04 11:49:50 -0400

    Loop with email, property and tasks should be up.

--- scripts/scheduled-tasks.js
+++ scripts/scheduled-tasks.js
@@ -10,14 +10,38 @@ var schedule = require('node-schedule'),
     rule = new schedule.RecurrenceRule(),
     rule2 = new schedule.RecurrenceRule(),
     mongoose = require('mongoose'),
-    Article = mongoose.model('Article');
+    Article = mongoose.model('Article'),
+    Task = mongoose.model('Task'),
+    User = mongoose.model('User');
+
 
 rule.dayOfWeek = [0, new schedule.Range(2, 6)];
 rule.hour = 5;
 rule.minute = 0;
 
+
 var j = schedule.scheduleJob(rule, function(){
-    /*insert code here*/
+    var today = new Date(); /*set today to the date. Inside the function to constantly reset today*/
+
+    db.Task.aggregate(
+        [
+            {
+                $match : {due: today, complete: false} /* Due today and not completed*/
+            },
+            {
+                $group: {email: "$resUID.email", property: "$property.street", tasks: {$push: "$name"}} /*group by ID, then by property (for agents with tasks from different properties, then array the tasks for each property*/
+
+            }
+        ],
+        function(err, result){
+            for(var email in result){         /*loop through the object by email*/
+
+                } )
+
+
+
+        }
+    )
 });
 
 
@@ -25,7 +49,7 @@ var j = schedule.scheduleJob(rule, function(){
 
 /*weekly on Monday at 5am*/
 
-rule2.dayOfWeek = [0, new schedule.Range(1, 6)];
+rule2.dayOfWeek = 1;
 rule2.hour = 5;
 rule2.minute = 0;
 
