66fe3bc9af027e64bc399cfbe14c197040724efd
Bob Caudill <Bob Caudill>
2017-04-16 08:36:27 -0400

    Added automailer into articles.server.controller

--- modules/articles/server/controllers/articles.server.controller.js
+++ modules/articles/server/controllers/articles.server.controller.js
@@ -6,6 +6,10 @@
 var path = require('path'),
   mongoose = require('mongoose'),
   Article = mongoose.model('Article'),
+  Task = mongoose.model('Task'),
+  schedule = require('node-schedule'),
+  rule = new schedule.RecurrenceRule(),
+  rule2 = new schedule.RecurrenceRule(),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
   
   var _ = require('lodash'),
@@ -196,3 +200,110 @@ exports.articleByID = function (req, res, next, id) {
     next();
   });
 };
+
+/*Auto Mailer
+* Daily and weekly announcements. Debug currently sends emails to Bob Caudill. Please switch
+* email before testing.*/
+/*
+ rule.dayOfWeek = [0, new schedule.Range(2, 6)];
+ rule.hour = 5;
+ rule.minute = 0;
+ */
+/*DEBUG*/ rule.minute = new schedule.Range(0, 59, 10);
+
+schedule.scheduleJob(rule, function(){
+    var today = new Date(); /*set today to the date. Inside the function to constantly reset today*/
+    db.Task.aggregate(
+        [
+            /* Due today and not completed*/
+            {$match : {due: today, complete: false}},
+            /*group by ID, then by property (for agents with tasks from different properties, then array the tasks for each property*/
+            {$group: {email: "$resUID.email", property: "$property.street", subscribed:"$resUID.subscribed", tasks: {$push: "$name"}}}
+        ],
+        function(err, result){
+            for(var email in result) {
+                if (result.subscribed = true) {
+                    var tasklist = "";
+                    for(var task in result.tasks){
+                        tasklist = task + " ";
+                    }
+                    /*loop through the object by email*/
+                    /*DEBUG*/var email="gregoryallencaudill@gmail.com";
+                    var passage = "<!DOCTYPE html><html lang='en-US'><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head>" +
+                        "<body><h3>Hello!</h3>" +
+                        "<p>You have a task(s) due today for the property at " + result.property + ": </p>" +
+                        tasklist +
+                        "<p>If you have already completed this task(s), please sign in at way2close.herokuapp.com/settings/profile" +
+                        " and mark the task as complete. If you would like to stop receiving notices from Way2Close, please follow" +
+                        " this link " + link +
+                        "<h3>Regards,</h3><h4>The Way2Close Team</h4></body>" +
+                        "<footer>Please do not reply to this email.</footer>" +
+                        "</html>";
+                    transporter.sendMail({
+                        from: "way2closeapp@gmail.com",
+                        to: email,
+                        subject: 'Upcoming task from Way2Close',
+                        html: passage
+                    }, (err, info) => {
+                        if(err) {console.log("62 email " + err);}
+                        else {console.log("message sent successfully to: " + email);}
+                });
+                }
+
+            }
+        }
+    )
+});
+
+
+
+
+/*weekly on Monday at 5am*/
+
+rule2.dayOfWeek = 1;
+rule2.hour = 5;
+rule2.minute = 0;
+
+var j2 = schedule2.scheduleJob(rule2, function(){
+    var startDate = new Date(), /*set today to the date. Inside the function to constantly reset today*/
+        endDate = new Date(startDate.getTime() + 7 * 24 * 60 * 60 * 1000)
+    db.Task.aggregate(
+        [
+            /* Due today and not completed*/
+            {$match : {due: {$gt: startDate, $lt: endDate}, complete: false}},
+            /*group by ID, then by property (for agents with tasks from different properties, then array the tasks for each property*/
+            {$group: {email: "$resUID.email", property: "$property.street", subscribed:"$resUID.subscribed", tasks: {$push: "$name"}}}
+        ],
+        function(err, result){
+            for(var email in result) {
+                if (result.subscribed = true) {
+                    var tasklist = "";
+                    for(var task in result.tasks){
+                        tasklist = task + " ";
+                    }
+                    /*loop through the object by email*/
+                    var passage = "<!DOCTYPE html><html lang='en-US'><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head>" +
+                        "<body><h3>Hello!</h3>" +
+                        "<p>You have a task(s) due this week for the property at " + result.property + ": </p>" +
+                        tasklist +
+                        "<p>If you have already completed this task(s), please sign in at way2close.herokuapp.com/settings/profile" +
+                        " and mark the task as complete. If you would like to stop receiving notices from Way2Close, please follow" +
+                        " this link " + link +
+                        "<h3>Regards,</h3><h4>The Way2Close Team</h4></body>" +
+                        "<footer>Please do not reply to this email.</footer>" +
+                        "</html>";
+                    transporter.sendMail({
+                        from: "way2closeapp@gmail.com",
+                        to: email,
+                        subject: 'Weekly update from Way2Close',
+                        html: passage
+                    }, (err, info) => {
+                        if(err) {console.log("62 email " + err);}
+                        else {console.log("message sent successfully to: " + email);}
+                });
+                }
+
+            }
+        }
+    )
+});
--- scripts/scheduled-tasks.js
+++ scripts/scheduled-tasks.js
@@ -14,13 +14,14 @@ var schedule = require('node-schedule'),
     Task = mongoose.model('Task'),
     User = mongoose.model('User');
 
-
+/*
 rule.dayOfWeek = [0, new schedule.Range(2, 6)];
 rule.hour = 5;
 rule.minute = 0;
+*/
+/*DEBUG*/ rule.minute = new schedule.Range(0, 59, 10);
 
-
-var j = schedule.scheduleJob(rule, function(){
+schedule.scheduleJob(rule, function(){
     var today = new Date(); /*set today to the date. Inside the function to constantly reset today*/
     db.Task.aggregate(
         [
@@ -37,6 +38,7 @@ var j = schedule.scheduleJob(rule, function(){
                         tasklist = task + " ";
                     }
                     /*loop through the object by email*/
+                    /*DEBUG*/var email="gregoryallencaudill@gmail.com";
                     var passage = "<!DOCTYPE html><html lang='en-US'><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head>" +
                         "<body><h3>Hello!</h3>" +
                         "<p>You have a task(s) due today for the property at " + result.property + ": </p>" +
@@ -73,5 +75,45 @@ rule2.hour = 5;
 rule2.minute = 0;
 
 var j2 = schedule2.scheduleJob(rule2, function(){
-    /*insert code here*/
+    var startDate = new Date(), /*set today to the date. Inside the function to constantly reset today*/
+        endDate = new Date(startDate.getTime() + 7 * 24 * 60 * 60 * 1000)
+    db.Task.aggregate(
+        [
+            /* Due today and not completed*/
+            {$match : {due: {$gt: startDate, $lt: endDate}, complete: false}},
+            /*group by ID, then by property (for agents with tasks from different properties, then array the tasks for each property*/
+            {$group: {email: "$resUID.email", property: "$property.street", subscribed:"$resUID.subscribed", tasks: {$push: "$name"}}}
+        ],
+        function(err, result){
+            for(var email in result) {
+                if (result.subscribed = true) {
+                    var tasklist = "";
+                    for(var task in result.tasks){
+                        tasklist = task + " ";
+                    }
+                    /*loop through the object by email*/
+                    var passage = "<!DOCTYPE html><html lang='en-US'><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head>" +
+                        "<body><h3>Hello!</h3>" +
+                        "<p>You have a task(s) due this week for the property at " + result.property + ": </p>" +
+                        tasklist +
+                        "<p>If you have already completed this task(s), please sign in at way2close.herokuapp.com/settings/profile" +
+                        " and mark the task as complete. If you would like to stop receiving notices from Way2Close, please follow" +
+                        " this link " + link +
+                        "<h3>Regards,</h3><h4>The Way2Close Team</h4></body>" +
+                        "<footer>Please do not reply to this email.</footer>" +
+                        "</html>";
+                    transporter.sendMail({
+                        from: "way2closeapp@gmail.com",
+                        to: email,
+                        subject: 'Weekly update from Way2Close',
+                        html: passage
+                    }, (err, info) => {
+                        if(err) {console.log("62 email " + err);}
+                        else {console.log("message sent successfully to: " + email);}
+                });
+                }
+
+            }
+        }
+    )
 });
